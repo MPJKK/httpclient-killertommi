3a6fb154ccc42e4cfb58ee9a7884f9c4c7aca81c
killertommi <tommi.2525@hotmail.com>
2018-01-18 14:14:12 +0200

    second commit

--- src/app/http-test/http-test.component.ts
+++ src/app/http-test/http-test.component.ts
@@ -1,32 +1,32 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {HttpClient} from '@angular/common/http';
 
 @Component({
-  selector: 'app-http-test',
-  templateUrl: './http-test.component.html',
-  styleUrls: ['./http-test.component.scss']
+    selector: 'app-http-test',
+    templateUrl: './http-test.component.html',
+    styleUrls: ['./http-test.component.scss']
 })
 export class HttpTestComponent implements OnInit {
 
-  tulos = 'Moro';
+    tulos = 'Moro';
 
-  constructor(private http: HttpClient) {
+    constructor(private http: HttpClient) {
 
-  }
-
-  getJson () {
-    interface Myinterface {
-      license: string;
     }
 
-    this.http.get<Myinterface>('assets/package.json').subscribe( data => {
-      console.log(data);   // tarkastellaan sisältöä consolessa
-        this.tulos = data.license;
-    });
-  }
+    getJson() {
+        interface Myinterface {
+            license: string;
+        }
 
-  ngOnInit() {  // kutsutaan täällä metodia getJson
-      this.getJson();
-  }
+        this.http.get<Myinterface>('assets/package.json').subscribe(data => {
+            console.log(data);   // tarkastellaan sisältöä consolessa
+            this.tulos = data.license;
+        });
+    }
+
+    ngOnInit() {  // kutsutaan täällä metodia getJson
+        this.getJson();
+    }
 
 }
